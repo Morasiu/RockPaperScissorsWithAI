cad9b0df3c8b7a3b8e17667766cd4a6b9cd0c4e7
Poted <o.jpkmc@gmail.com>
2019-02-04 21:48:27 +0100

    more tests.

--- Project/RPSClassLib.Tests/RSPGameTest.cs
+++ Project/RPSClassLib.Tests/RSPGameTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
 using RPS;
 
 namespace RPSClassLib.Tests
@@ -13,7 +15,7 @@ namespace RPSClassLib.Tests
             var computerPick = rpsGame.GetComputerPick(playerPick);
             var winner = rpsGame.Rounds[0].Winner;
             var expectedScore = new Score();
-            
+
             if (playerPick == computerPick)
             {
                 expectedScore.ComputerScore = 0;
@@ -32,7 +34,27 @@ namespace RPSClassLib.Tests
             Assert.Equal(expectedScore.HumanScore, rpsGame.Score.HumanScore);
             Assert.Equal(expectedScore.ComputerScore, rpsGame.Score.ComputerScore);
         }
-        
+
+        [Fact]
+        public void Rounds_ThirdRound_ShouldReturnRoundProperRoundValue()
+        {
+            var rpsGame = new RPSGame();
+            var playerPick = Pick.Paper;
+            var eachRound = rpsGame.GetComputerPick(playerPick);
+            eachRound = rpsGame.GetComputerPick(playerPick);
+            eachRound = rpsGame.GetComputerPick(playerPick);
+
+            int gamePlayCount = new int();
+
+            foreach (var round in rpsGame.Rounds)
+            {
+                gamePlayCount++;
+            }
+            var howManyRounds = rpsGame.Rounds.Count;
+
+            Assert.Equal(gamePlayCount, howManyRounds);
+        }
+
         [Fact]
         public void Winner_TestRoundFirst_ShouldReturnWinner()
         {
@@ -58,6 +80,7 @@ namespace RPSClassLib.Tests
             }
 
             Assert.Equal(expectedWinner, winner);
+
         }
 
         [Fact]
@@ -66,9 +89,61 @@ namespace RPSClassLib.Tests
             var rpsGame = new RPSGame();
             var playerPick = Pick.Paper;
             var computerPick = rpsGame.GetComputerPick(playerPick);
-             computerPick = rpsGame.GetComputerPick(playerPick);
+            computerPick = rpsGame.GetComputerPick(playerPick);
 
             Assert.Equal(Pick.Rock, computerPick);
+
         }
+
+        [Fact]
+        public void CurrentRound_SecondRound_ShouldReturnOppositePick()
+        {
+            var rpsGame = new RPSGame();
+            var playerPick = Pick.Paper;
+            var eachRound = rpsGame.GetComputerPick(playerPick);
+            eachRound = rpsGame.GetComputerPick(playerPick);
+
+            var currentRound = rpsGame.CurrentRound.ComputerPick;
+
+            Assert.NotEqual(playerPick, currentRound);
+
+        }
+
+
+        [Fact]
+        public void IsGameOver()
+        {
+            var rpsGame = new RPSGame();
+            var playerPick = Pick.Paper;
+            var computerPick = rpsGame.GetComputerPick(playerPick);
+
+            do
+            {
+                computerPick = rpsGame.GetComputerPick(playerPick);
+            }
+
+            while (rpsGame.IsGameOver != true);
+
+
+            Assert.Throws<ApplicationException>(() => rpsGame.GetComputerPick(playerPick));
+        }
+
+        public static IEnumerable<object[]> enumValues()
+        {
+            foreach (var player in Enum.GetValues(typeof(Player)))
+                yield return new object[] { player };
+
+        }
+        [Theory]
+        [MemberData(nameof(enumValues))]
+        public void Player_TestRound_EnumShouldBeSetToProperNumber(Player player)
+        {
+
+            Assert.NotNull(player);
+            
+        }
+
+
+
     }
 }
